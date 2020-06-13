2f9b80050f971a6f25d4d4c732df98eb553058b6
Jeongwoo Choi <iamchoij@gmail.com>
2020-06-13 13:02:36 -0500

    Added multiple forms

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Item from './components/Item/Item.js';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Item from "./components/Item/Item.js";
+import Result from './components/Result/Result.js';
 
 function App() {
-  return (
-    <div className="App">
-      <Item name="Food" value="0"/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div id="items">
+				<Item name="Canned Food" value="0" />
+				<Item name="Toliet Paper" value="0" />
+				<Item name="Hand Sanitizer" value="0" />
+			</div>
+			<div id="results">
+        <h1>Results</h1>
+        <Result />
+			</div>
+		</div>
+	);
 }
 
 export default App;
