1d99fe3b0ae7d23617cd7a80400dc8da6c8b1e7b
Jeongwoo Choi <iamchoij@gmail.com>
2020-06-14 19:21:47 -0500

    Fixed UI/bootstrap

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import boatImage from '../src/boatImage.png';
-
-const Styles = styled.div`
-  .jumbo {
-    background: url(${boatImage}) no-repeat fixed bottom;
-    background-size: cover;
-    color: #efefef;
-    height: 200px;
-    position: relative;
-    z-index: -2;
-  }
-  .overlay {
-    background-color: #000;
-    opacity: 0.6;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: -1;
-  }
-`;
+import "./Jumbotron.css";
 
 export const Jumbotron = () => (
-  <Styles>
     <Jumbo fluid className="jumbo">
       <div className="overlay"></div>
       <Container>
@@ -32,5 +11,4 @@ export const Jumbotron = () => (
         <p>COVID-AID</p>
       </Container>
     </Jumbo>
-  </Styles>
 )
